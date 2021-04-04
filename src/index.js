6aeb40f29e410a199374e78c2fed8a1a37fbd85d
Tyler Morales <moratyle@gmail.com>
2021-04-04 15:11:44 -0500

    📦  Add Tailwind CSS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import "./index.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app.js";
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './app.js'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.querySelector("#root")
-);
+  document.querySelector('#root')
+)
