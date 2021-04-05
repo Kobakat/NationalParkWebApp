275be05a2f5d13a359f54b2c53deace00e9d6440
Tyler Morales <moratyle@gmail.com>
2021-04-04 23:15:23 -0500

    📱  Make responsive

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,21 +9,24 @@ const SearchBar = () => {
     { label: 'Arcadia National Park', value: '2' },
   ]
 
-  const [selected, setSelected] = useState([])
+  const [selectedParks, setSelectedParks] = useState([])
+  const [selectedStates, setSelectedStates] = useState([])
+  const [selectedActivities, setSelectedActivities] = useState([])
+  const [selectedTopics, setSelectedTopics] = useState([])
 
   return (
     <div class="bg-white min-h-28 w-4/5 m-auto py-8 px-8 rounded-2xl border-2 border-gray-100 shadow-lg">
       <Tabs>
         <div class="border-b-2">
           <TabList>
-            <div class="flex gap-2 pb-4">
+            <div class="flex gap-2 pb-4 flex-col lg:flex-row text-center">
               <Tab selectedClassName="bg-green-700 text-white rounded-lg">
-                <button class="py-4 px-10 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer w-42 transition">
+                <button class="py-4 px-10 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer w-full transition">
                   Park Name
                 </button>
               </Tab>
               <Tab selectedClassName="bg-green-700 text-white rounded-lg">
-                <button class="py-4 px-10 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer w-42 transition">
+                <button class="py-4 px-10 rounded-lg hover:bg-green-500 hover:text-white cursor-pointer w-full transition">
                   Filter
                 </button>
               </Tab>
@@ -36,12 +39,12 @@ const SearchBar = () => {
             <label class="text-green-700" htmlFor="park-name">
               Park Name
             </label>
-            <div class="flex gap-4">
+            <div class="flex gap-4 flex-col lg:flex-row">
               <MultiSelect
                 class="bg-gray-100 p-3 w-full rounded-3xl pl-6 focus:outline-none focus:ring-2 focus:ring-blue-400"
                 options={options}
-                value={selected}
-                onChange={setSelected}
+                value={selectedStates}
+                onChange={setSelectedStates}
                 labelledBy="Select"
               />
               <button class="text-white bg-green-700 hover:bg-green-500 focus:bg-green-500 py-4 px-10 rounded-lg transition">
@@ -52,7 +55,7 @@ const SearchBar = () => {
         </TabPanel>
 
         <TabPanel>
-          <div class="flex gap-4 pt-6 items-end">
+          <div class="flex gap-4 pt-6 items-end flex-col lg:flex-row">
             <div class="flex flex-col gap-4 w-full content-end">
               {/* state */}
               <label class="text-green-700" htmlFor="park-name">
@@ -62,8 +65,8 @@ const SearchBar = () => {
                 <MultiSelect
                   class="bg-gray-100 p-3 w-full rounded-3xl pl-6 focus:outline-none focus:ring-2 focus:ring-blue-400"
                   options={options}
-                  value={selected}
-                  onChange={setSelected}
+                  value={selectedParks}
+                  onChange={setSelectedParks}
                   labelledBy="Select"
                 />
               </div>
@@ -77,8 +80,8 @@ const SearchBar = () => {
                 <MultiSelect
                   class="bg-gray-100 p-3 w-full rounded-3xl pl-6 focus:outline-none focus:ring-2 focus:ring-blue-400"
                   options={options}
-                  value={selected}
-                  onChange={setSelected}
+                  value={selectedActivities}
+                  onChange={setSelectedActivities}
                   labelledBy="Select"
                 />
               </div>
@@ -92,8 +95,8 @@ const SearchBar = () => {
                 <MultiSelect
                   class="bg-gray-100 p-3 w-full rounded-3xl pl-6 focus:outline-none focus:ring-2 focus:ring-blue-400"
                   options={options}
-                  value={selected}
-                  onChange={setSelected}
+                  value={selectedTopics}
+                  onChange={setSelectedTopics}
                   labelledBy="Select"
                 />
               </div>
