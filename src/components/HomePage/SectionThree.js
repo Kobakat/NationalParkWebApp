775b2b7291995274162ee1416f23f26cc37c92f2
Sneh Mehta <smehta@colum.edu>
2021-04-12 18:24:09 -0500

    🎠Carousel for Section 3 Implemented using Swiper JS
    Is not responsive yet. Also needs a link to for the button that is there. As well as changing the text per the image that is currently showing

--- src/components/HomePage/SectionThree.js
+++ src/components/HomePage/SectionThree.js
@@ -1,7 +1,63 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Pagination } from "swiper";
 import "./section-three.css";
+import "swiper/swiper.scss";
+
+SwiperCore.use([Autoplay, Pagination]);
 
 function SectionThree() {
-  return <section className="carousel-section">Section 3</section>;
+  return (
+    <section className="carousel-section">
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 3000 }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide className="carousel-slide">
+          <img
+            src="https://randomwordgenerator.com/img/picture-generator/55e0d54b4a51ad14f1dc8460962e33791c3ad6e04e507441722a72dc9345c4_640.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide className="carousel-slide">
+          <img
+            src="https://randomwordgenerator.com/img/picture-generator/55e0d54b4a51ad14f1dc8460962e33791c3ad6e04e507441722a72dc9345c4_640.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide className="carousel-slide">
+          <img
+            src="https://randomwordgenerator.com/img/picture-generator/55e0d54b4a51ad14f1dc8460962e33791c3ad6e04e507441722a72dc9345c4_640.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide className="carousel-slide">
+          <img
+            src="https://randomwordgenerator.com/img/picture-generator/55e0d54b4a51ad14f1dc8460962e33791c3ad6e04e507441722a72dc9345c4_640.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide className="carousel-slide">
+          <img
+            src="https://randomwordgenerator.com/img/picture-generator/55e0d54b4a51ad14f1dc8460962e33791c3ad6e04e507441722a72dc9345c4_640.jpg"
+            alt=""
+          />
+        </SwiperSlide>
+      </Swiper>
+      <div className="carousel-text-container">
+        <h2 className="carousel-title">Lorem ipsum dolor sit amet.</h2>
+        <p className="carousel-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+          explicabo!
+        </p>
+        {/* add the linkto */}
+        <button className="carousel-button">Lorem</button>
+      </div>
+    </section>
+  );
 }
 export default SectionThree;
