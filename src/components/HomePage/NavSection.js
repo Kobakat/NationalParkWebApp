47b090a946a66558e33d964dd88fdc0ae3f7b08d
Tyler Morales <moratyle@gmail.com>
2021-04-12 11:28:48 -0600

    🚚  Add routes to nav links

--- src/components/HomePage/NavSection.js
+++ src/components/HomePage/NavSection.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ParkwayLogo from "../HomePage/images/ParkwayLogo.svg";
-import "./section-nav.css";
+import React from 'react'
+import ParkwayLogo from '../HomePage/images/ParkwayLogo.svg'
+import './section-nav.css'
 
 function Nav() {
   return (
@@ -8,18 +8,18 @@ function Nav() {
       <ul className="nav-bar">
         <div className="links-container">
           <li className="logo">
-            <a href="">
+            <a href="/">
               <img src={ParkwayLogo} alt="Logo" />
             </a>
           </li>
           <li className="nav-links">
-            <a href="">About</a>
+            <a href="/about">About</a>
           </li>
           <li className="nav-links">
-            <a href="">Parks</a>
+            <a href="/parks">Parks</a>
           </li>
           <li className="nav-links">
-            <a href="">Log in</a>
+            <a href="/log-in">Log in</a>
           </li>
           <li className="nav-links">
             <a className="btn-cta" href="">
@@ -29,6 +29,6 @@ function Nav() {
         </div>
       </ul>
     </nav>
-  );
+  )
 }
-export default Nav;
+export default Nav
