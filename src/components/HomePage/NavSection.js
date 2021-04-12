d46a8da92038bfd7d33ffd25b5061e1418f26272
Tyler Morales <44333523+tyler-morales@users.noreply.github.com>
2021-04-12 11:31:24 -0600

    Update nav links (#28)
    * 🚚  Add routes to nav links
    
    * 🗑 Delete duplicate selection variable decleration

--- src/components/HomePage/NavSection.js
+++ src/components/HomePage/NavSection.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ParkwayLogo from "../HomePage/images/ParkwayLogo.svg";
-import "./section-nav.css";
+import React from 'react'
+import ParkwayLogo from '../HomePage/images/ParkwayLogo.svg'
+import './section-nav.css'
 
 function Nav() {
   return (
@@ -8,18 +8,18 @@ function Nav() {
       <ul className="nav-bar">
         <div className="links-container">
           <li className="logo">
-            <a href="https://nps.gov">
+            <a href="/">
               <img src={ParkwayLogo} alt="Logo" />
             </a>
           </li>
           <li className="nav-links">
-            <a href="https://nps.gov">About</a>
+            <a href="/about">About</a>
           </li>
           <li className="nav-links">
-            <a href="https://nps.gov">Parks</a>
+            <a href="/parks">Parks</a>
           </li>
           <li className="nav-links">
-            <a href="https://nps.gov">Log in</a>
+            <a href="/log-in">Log in</a>
           </li>
           <li className="nav-links">
             <a className="btn-cta" href="https://nps.gov">
@@ -29,6 +29,6 @@ function Nav() {
         </div>
       </ul>
     </nav>
-  );
+  )
 }
-export default Nav;
+export default Nav
