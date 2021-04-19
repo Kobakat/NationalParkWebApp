572df88548d3a4cecf248c32c58dc42314303dab
Sneh Mehta <smehta@colum.edu>
2021-04-19 15:20:15 -0500

    📷✂️Compressed and Optimized Images Used Throughout The Project
    Tiny PNG and some SVG compressor

--- src/components/HomePage/HeroSection.js
+++ src/components/HomePage/HeroSection.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchBar from "../SearchBar";
 import "./section-hero.css";
-import HeroPicture from "./images/yosemitevalley (1).png"
+import HeroPicture from "./images/yosemitevalley.png";
 
 function HeroSection() {
-  return <main>
-    <SearchBar/> 
-    <img className="landing-image"src = {HeroPicture}alt="" />
-  </main>;
+  return (
+    <main>
+      <SearchBar />
+      <img className="landing-image" src={HeroPicture} alt="" />
+    </main>
+  );
 }
 export default HeroSection;
