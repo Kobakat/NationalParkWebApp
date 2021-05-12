5ac08ddb1897758ab3b302d0e9c704d25f1369a5
Sneh Mehta <smehta@colum.edu>
2021-05-11 19:08:35 -0500

    mapbox fix test
    testing a fix for mapbox not loading

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   style: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
-}
+  babel: {
+    loaderOptions: {
+      ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
+    },
+  },
+};
